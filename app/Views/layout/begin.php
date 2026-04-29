550c2e2521c01b2c02cf8c66c044db05230817aa
Sztivi-DK <szalontai.istvan@gmail.com>
2026-04-29 12:33:02 +0200

    header done with CSS

--- app/Views/layout/begin.php
+++ app/Views/layout/begin.php
@@ -5,33 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web01 Reloaded</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="ckeditor5/ckeditor5.css">
+    <link rel="stylesheet" href="<?= base_url('style.css') ?>">
 </head>
 <body>
 
     <header>
-        <div class="row">
-            <div>Web01 Reloaded Header</div>
+        <div class="container">
+	    <div>
+		<h1>Rákattantam a gyakorlásra</h1>
+		<p>A munka kitölti a rendelkezésére fennálló időt.</p>
+	    </div>
             <nav>
-            <?php if (true): ?>
-		<ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="<?= base_url() ?>" class="nav-link">
+		<ul>
+                    <li>
+                        <a href="<?= base_url() ?>">
                             <i class="fa-solid fa-house-chimney"></i> Kezdőlap
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a href="<?= base_url('services') ?>" class="nav-link">
+                    <li>
+                        <a href="<?= base_url('services') ?>">
                             <i class="fa-solid fa-folder-open"></i> Szolgáltatások
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a href="<?= base_url('contact') ?>" class="nav-link">
-                        <i class="fa-solid fa-rectangle-list"></i> Kapcsolat
+                    <li>
+                        <a href="<?= base_url('contact') ?>">
+			    <i class="fa-solid fa-rectangle-list"></i> Kapcsolat
                         </a>
                     </li>
                 </ul>
-	    <?php endif; ?>
             </nav>
         </div>
     </header>
